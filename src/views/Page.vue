040e439d5e805229e88e9f8eef480edeaf39beaf
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2023-07-16 12:13:54 +0200

    refactor: remove VPage component by moving the functionality to the route

--- src/views/Page.vue
+++ src/views/Page.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
-import VPage from "@/components/VPage.vue";
+import VEmpty from "@/components/VEmpty.vue";
+import VPageItem from "@/components/VPageItem.vue";
+import VTextarea2 from "@/components/VTextarea2.vue";
+import { Item, TaskStatus, parse } from "@/lib/parser";
+import { continueListRules, type ContinueListRule } from "@/lib/text";
+import { usePage } from "@/stores/page";
+import { FileX2 } from "lucide-vue-next";
 import { computed, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 
@@ -11,13 +17,68 @@ const route = useRoute();
 
 const pageId = computed(() => route.params.id?.toString());
 
-const editorEl = ref<InstanceType<typeof VPage> | null>(null);
+const { exists, text, updateItem } = usePage(() => pageId.value);
+
+// @ts-expect-error Vue types seem to be buggy here
+const textareaEl = ref<InstanceType<typeof VTextarea2> | null>(null);
 
 watch(pageId, () => {
-  editorEl.value?.focus();
+  textareaEl.value?.focus(0);
 });
+
+/* -------------------------------------------------- *
+ * Editor hooks and customizations                    *
+ * -------------------------------------------------- */
+
+function rowToTask(row: string): Item {
+  return parse(row);
+}
+
+const continueLists: ContinueListRule[] = [
+  { pattern: /\t*\[-] /, next: "same" },
+  { pattern: /\t*\[.\] /, next: (match) => match.replace(/\[.\]/, "[ ]") },
+  ...Object.values(continueListRules),
+];
+
+/* -------------------------------------------------- *
+ * Interacting with items                             *
+ * -------------------------------------------------- */
+
+function updateStatus(index: number, status: TaskStatus) {
+  updateItem(index, (item) => {
+    item.status = status;
+  });
+}
 </script>
 
 <template>
-  <VPage :page-id="pageId" ref="editorEl" />
+  <VEmpty v-if="!exists" :icon="FileX2" text="This page doesn't exist." />
+
+  <VTextarea2
+    v-else-if="text !== undefined"
+    :class="$style.editor"
+    :context-provider="rowToTask"
+    :continue-lists="continueLists"
+    ref="textareaEl"
+    v-model="text"
+  >
+    <template #row="{ context, index }">
+      <VPageItem
+        :as="index === 0 ? 'heading' : undefined"
+        :item="context"
+        @update:status="updateStatus(index, $event)"
+      />
+    </template>
+  </VTextarea2>
 </template>
+
+<style module>
+.editor {
+  caret-color: var(--c-fg);
+  font-family: var(--font-mono);
+  margin: auto;
+  max-width: 50rem;
+  min-height: 100%;
+  padding-top: 0.25rem;
+}
+</style>
