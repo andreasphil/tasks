891dae6f15c9649af408942183d2d2ad09483af6
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2023-07-30 14:44:33 +0200

    refactor: simplify router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,21 @@
+import NotFound from "@/views/NotFound.vue";
+import Page from "@/views/Page.vue";
+import Pages from "@/views/Pages.vue";
+import Welcome from "@/views/Welcome.vue";
 import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: "/",
-    name: "Home",
-    redirect: { name: "Welcome" },
-  },
+  { path: "/", name: "Home", redirect: { name: "Welcome" } },
   {
     path: "/pages",
     name: "Pages",
-    component: () => import("@/views/Pages.vue"),
+    component: Pages,
     children: [
-      {
-        path: "",
-        name: "Welcome",
-        component: () => import("@/views/Welcome.vue"),
-      },
-      {
-        path: ":id",
-        name: "Page",
-        component: () => import("@/views/Page.vue"),
-      },
+      { path: "", name: "Welcome", component: Welcome },
+      { path: ":id", name: "Page", component: Page },
     ],
   },
-  {
-    path: "/:catchAll(.*)*",
-    name: "NotFound",
-    component: () => import("@/views/NotFound.vue"),
-  },
+  { path: "/:catchAll(.*)*", name: "NotFound", component: NotFound },
 ];
 
 const history = createWebHashHistory();
