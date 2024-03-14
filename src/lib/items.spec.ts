417fc6e8f019071ab19d24841894761b44360dc4
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2024-03-14 17:41:17 +0100

    feat: implement comparison of two items

--- src/lib/items.spec.ts
+++ src/lib/items.spec.ts
@@ -1,6 +1,6 @@
 import { Item, parse } from "@/lib/parser";
 import { describe, expect, it } from "vitest";
-import { asWritable, mutate } from "./items";
+import { asWritable, compare, mutate } from "./items";
 
 describe("writable items", () => {
   describe("set type", () => {
@@ -258,6 +258,226 @@ describe("writable items", () => {
       });
 
       expect(r.status).toBe("completed");
+    });
+  });
+
+  describe("comparing items", () => {
+    it("keeps the order of notes", () => {
+      const a = parse("Note 1");
+      const b = parse("Note 2");
+
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+    });
+
+    it("keeps the order of sections", () => {
+      const a = parse("# Section 1");
+      const b = parse("# Section 2");
+
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+    });
+
+    it("keeps the order of notes and sections", () => {
+      const a = parse("Note 1");
+      const b = parse("# Section 1");
+
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+    });
+
+    it("keeps the order of notes and tasks", () => {
+      const a = parse("Note 1");
+      const b = parse("[ ] Task 1");
+
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+    });
+
+    it("keeps the order of sections and tasks", () => {
+      const a = parse("# Section 1");
+      const b = parse("[ ] Task 1");
+
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+    });
+
+    it("keeps the order of notes and blank lines", () => {
+      const a = parse("Note 1");
+      const b = parse("");
+
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+    });
+
+    it("keeps the order of sections and blank lines", () => {
+      const a = parse("# Section 1");
+      const b = parse("");
+
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+    });
+
+    it("keeps the order of tasks and blank lines", () => {
+      const a = parse("[ ] Task 1");
+      const b = parse("");
+
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+    });
+
+    it("sorts by due date when both have due dates", () => {
+      const a = parse("[ ] Task 1 ->2021-01-01");
+      const b = parse("[ ] Task 2 ->2022-01-01");
+
+      expect(compare(a, b)).toBeLessThan(0);
+      expect(compare(b, a)).toBeGreaterThan(0);
+    });
+
+    it("sorts by status when both have identical due dates", () => {
+      const a = parse("[ ] Task 1 ->2021-01-01");
+      const b = parse("[x] Task 2 ->2021-01-01");
+
+      expect(compare(a, b)).toBeLessThan(0);
+      expect(compare(b, a)).toBeGreaterThan(0);
+    });
+
+    it("sorts by due date when only one has a due date", () => {
+      const a = parse("[ ] Task 1 ->2021-01-01");
+      const b = parse("[ ] Task 2");
+
+      expect(compare(a, b)).toBeLessThan(0);
+      expect(compare(b, a)).toBeGreaterThan(0);
+    });
+
+    it("keeps the order for equal due dates", () => {
+      const a = parse("[ ] Task 1 ->2021-01-01");
+      const b = parse("[ ] Task 2 ->2021-01-01");
+
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+    });
+
+    it("keeps the order for equal due dates and status", () => {
+      const a = parse("[ ] Task 1 ->2021-01-01");
+      const b = parse("[ ] Task 2 ->2021-01-01");
+
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+    });
+
+    it("sorts by status", () => {
+      const incomplete = parse("[ ] Task 1");
+      const completed = parse("[x] Task 2");
+      const inProgress = parse("[/] Task 3");
+      const question = parse("[?] Task 4");
+      const important = parse("[*] Task 5");
+
+      const expectedResults = [
+        [
+          [incomplete, completed],
+          [incomplete, completed],
+        ],
+        [
+          [incomplete, inProgress],
+          [inProgress, incomplete],
+        ],
+        [
+          [incomplete, question],
+          [incomplete, question],
+        ],
+        [
+          [incomplete, important],
+          [important, incomplete],
+        ],
+        [
+          [completed, inProgress],
+          [inProgress, completed],
+        ],
+        [
+          [completed, question],
+          [question, completed],
+        ],
+        [
+          [completed, important],
+          [important, completed],
+        ],
+        [
+          [inProgress, question],
+          [inProgress, question],
+        ],
+        [
+          [inProgress, important],
+          [important, inProgress],
+        ],
+        [
+          [question, important],
+          [important, question],
+        ],
+      ];
+
+      expectedResults.forEach(([items, result]) => {
+        expect([...items].sort(compare)).toStrictEqual(result);
+      });
+    });
+
+    it("keeps the order for equal status", () => {
+      const a = parse("[ ] Task 1");
+      const b = parse("[ ] Task 2");
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+
+      const c = parse("[x] Task 1");
+      const d = parse("[x] Task 2");
+      expect(compare(c, d)).toBe(0);
+      expect(compare(d, c)).toBe(0);
+
+      const e = parse("[/] Task 1");
+      const f = parse("[/] Task 2");
+      expect(compare(e, f)).toBe(0);
+      expect(compare(f, e)).toBe(0);
+
+      const g = parse("[?] Task 1");
+      const h = parse("[?] Task 2");
+      expect(compare(g, h)).toBe(0);
+      expect(compare(h, g)).toBe(0);
+
+      const i = parse("[*] Task 1");
+      const j = parse("[*] Task 2");
+      expect(compare(i, j)).toBe(0);
+      expect(compare(j, i)).toBe(0);
+    });
+
+    it("keeps the order when the leading whitespace is different", () => {
+      const a = parse("\t[ ] Task 1");
+      const b = parse("\t\t[*] Task 2");
+      expect(compare(a, b)).toBe(0);
+      expect(compare(b, a)).toBe(0);
+
+      const c = parse("\t[ ] Task 1 ->2021-01-01");
+      const d = parse("\t\t[ ] Task 2 ->2022-01-01");
+      expect(compare(c, d)).toBe(0);
+      expect(compare(d, c)).toBe(0);
+    });
+
+    it("does not change how items are grouped into paragraphs", () => {
+      const items = [
+        parse("Title"),
+        parse(""),
+        parse("[x] Task 1"),
+        parse(""),
+        parse("[ ] Task 2"),
+      ];
+
+      console.log(
+        [...items].sort((a, b) => {
+          const result = compare(a, b);
+          console.log({ a: a.raw, b: b.raw, result });
+          return result;
+        })
+      );
+
+      expect([...items].sort(compare)).toStrictEqual(items);
     });
   });
 });
--- src/lib/items.ts
+++ src/lib/items.ts
@@ -8,6 +8,11 @@ import {
   type TaskStatus,
   type UncheckedItem,
 } from "@/lib/parser";
+import dayjs from "dayjs";
+
+/* -------------------------------------------------- *
+ * Mutating items                                     *
+ * -------------------------------------------------- */
 
 /**
  * Special variant of the Item where all properties that can safely be changed
@@ -109,4 +114,61 @@ export function mutate(item: Item, factory: (item: WritableItem) => void) {
   const w = asWritable(item);
   factory(w);
   Object.assign(item, w);
+}
+
+/* -------------------------------------------------- *
+ * Sorting items                                      *
+ * -------------------------------------------------- */
+
+const statusWeights: Record<TaskStatus, number> = {
+  important: 10000,
+  inProgress: 1000,
+  incomplete: 100,
+  question: 10,
+  completed: 1,
+} as const;
+
+/**
+ * Sorts 2 items by the following criteria:
+ *
+ * 1. Sections and notes are always equal, i.e. comparing two should not change
+ *    their order.
+ * 2. When comparing sections or notes with tasks, the order should also stay
+ *    the same.
+ * 3. Tasks are first sorted by their due dates (ascending; tasks with due
+ *    dates should come before tasks without due dates), then status (important,
+ *    in progress, open, question, done). Tasks with the same due date or
+ *    status should not change their order.
+ * 4. To accomodate nested tasks, white space at the beginning of the line
+ *    always takes precedence over all other criteria. Tasks with different
+ *    leading white space should never change order. Note that sorting an
+ *    entire page using `compare(item, item)` will mess up pages with nested
+ *    items, as those nested items need to be moved together with their parent
+ *    item. If you need to sort an entire page, use `sort(page)`.
+ * 5. The order of items and blank lines should not change.
+ *
+ * @param a
+ * @param b
+ */
+export function compare(a: Item, b: Item): number {
+  // Keep order of items of different types
+  if (a.type !== b.type || a.type !== "task" || b.type !== "task") return 0;
+
+  // Keep order of items with different leading white space
+  const aIndent = a.raw.match(/^\s*/)?.[0] ?? "";
+  const bIndent = b.raw.match(/^\s*/)?.[0] ?? "";
+  if (aIndent !== bIndent) return 0;
+
+  // Sort by due date
+  if (a.dueDate && !b.dueDate) return -1;
+  else if (b.dueDate && !a.dueDate) return 1;
+  else {
+    const aDueDate = dayjs((a as UncheckedItem).dueDate).startOf("day");
+    const bDueDate = dayjs((b as UncheckedItem).dueDate).startOf("day");
+    const diff = aDueDate.diff(bDueDate);
+    if (diff !== 0) return diff;
+  }
+
+  // Sort by status
+  return statusWeights[b.status] - statusWeights[a.status];
 }
