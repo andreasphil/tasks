9025c5013a9a15d6c88b741746ff6bdd038d1d91
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2024-03-30 12:03:43 +0100

    feat: automatically manage theme color

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import VBar from "@/components/VBar.vue";
+import { useThemeColor } from "@andreasphil/design-system";
+import { onMounted } from "vue";
 import { RouterView } from "vue-router";
+
+onMounted(() => {
+  useThemeColor();
+});
 </script>
 
 <template>
