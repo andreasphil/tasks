de0b4cf728c08df6ee26860b4589a46f77f6a8ce
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2024-04-19 13:45:57 +0200

    fix: restore broken welcome page layout

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,11 +3,11 @@ import { Sticker } from "lucide-vue-next";
 </script>
 
 <template>
-  <div data-with-fallback="emtpy">
+  <article data-with-fallback="emtpy">
     <div />
     <div data-when="empty">
       <Sticker />
       <p>Welcome!</p>
     </div>
-  </div>
+  </article>
 </template>
