012f1c418c79d168e21e5d7630b2194e69e1747c
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2024-04-19 13:37:03 +0200

    chore: bump dependencies

--- src/components/VDialog.vue
+++ src/components/VDialog.vue
@@ -33,45 +33,22 @@ const localOpen = computed({
 
 <template>
   <dialog
-    :class="$style.dialog"
     @close="localOpen = false"
     @keydown.esc.stop.prevent="localOpen = false"
     ref="dialogEl"
   >
-    <header v-if="title" :class="$style.header">
+    <header v-if="title">
       <strong>{{ title }}</strong>
     </header>
 
-    <div :class="$style.body" data-trim="both">
+    <div data-trim="both">
       <slot />
     </div>
 
-    <footer :class="$style.footer">
+    <footer>
       <slot name="footer">
         <button @click="localOpen = false">Confirm</button>
       </slot>
     </footer>
   </dialog>
 </template>
-
-<style module>
-.dialog {
-  max-width: min(90dvw, 30rem);
-  width: 100%;
-}
-
-.header {
-  font-size: var(--font-size-h5);
-  margin-bottom: 1rem;
-  text-transform: capitalize;
-}
-
-.footer {
-  border-top: var(--border-width) solid var(--c-border-variant);
-  display: flex;
-  gap: 0.5rem;
-  justify-content: flex-end;
-  margin-top: 1rem;
-  padding-top: 1rem;
-}
-</style>
