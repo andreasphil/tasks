5a7657b1588bae150cf9f424bebc87033a22ec87
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2025-07-26 19:17:13 +0200

    fix: update helpers from template

--- scripts/lib/html.ts
+++ scripts/lib/html.ts
@@ -1,3 +1,16 @@
-export function html(raw: TemplateStringsArray, ...values: any[]) {
-  return String.raw({ raw }, ...values);
+const tag = (strings: TemplateStringsArray, ...values: unknown[]) =>
+  String.raw({ raw: strings }, ...values);
+
+/**
+ * Helper for HTML template strings. The tag does nothing, but using it will
+ * allow syntax highlighting and formatting if your editor supports it.
+ */
+export const html = tag;
+
+/**
+ * Simple heuristic for detecting if an app is running in development (a.k.a
+ * served from localhost) or production (everything else).
+ */
+export function isDev() {
+  return window.location.hostname === "localhost";
 }
