3b8b4c488c95f3aa27fd901851b9b3e569602063
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2026-06-29 18:09:38 +0200

    test: fix broken test setup

--- test-setup.ts
+++ test-setup.ts
@@ -1,1 +1,5 @@
-import "temporal-polyfill/global";
+import { installImplementation } from "temporal-polyfill/shim";
+
+// force to use polyfill on node so we can manipulate the date in tests while
+// waiting for temporal support, see https://github.com/nodejs/node/issues/63369
+installImplementation();
